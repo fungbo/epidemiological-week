afde80c50fdadefe1de8834fc56c504b6299f14d
bfeng <fungbo@126.com>
2016-11-30 18:48:00 +0800

    Init the project and the first version

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import moment from 'moment';
+
+const YEAR = 'year';
+const SUNDAY = 0;
+const SATURDAY = 3;
+const NEXT_SUNDAY = 7;
+const WEEKDAYS = 7;
+const MAX_EPI_WEEKS_IN_ONE_YEAR = 52;
+const INVALID_VALUE = -1;
+
+export default function epi(dateStr, format = 'YYYY-MM-DD') {
+  const date = moment(dateStr, format);
+  if (!date.isValid()) {
+    throw new Error("date is invalid");
+  }
+
+  moment.locale('en');
+
+  const year = date.year();
+  const firstEpiWeekDay = getFirstEpiWeekDay(date);
+
+  if (date.isSameOrAfter(firstEpiWeekDay)) {
+    let duration;
+    if (firstEpiWeekDay.dayOfYear() === 1) {
+      duration = date.dayOfYear() - firstEpiWeekDay.dayOfYear() + firstEpiWeekDay.day();
+    } else {
+      duration = date.dayOfYear() - firstEpiWeekDay.dayOfYear();
+    }
+    const week = parseInt(duration / WEEKDAYS, 10) + 1;
+
+    if (week > MAX_EPI_WEEKS_IN_ONE_YEAR) {
+      const firstDayNextYear = moment({year: year + 1});
+      const firstEpiWeekDayNextYear = getFirstEpiWeekDay(firstDayNextYear);
+
+      return firstDayNextYear.isSame(firstEpiWeekDayNextYear) ?
+      {year: year + 1, week: 1} : {year: INVALID_VALUE, week: INVALID_VALUE};
+    }
+
+    return {year, week};
+  }
+
+  const firstDayLastYear = moment({year: year - 1});
+  const firstEpiWeekDayLastYear = getFirstEpiWeekDay(firstDayLastYear);
+
+  return firstDayLastYear.isSame(firstEpiWeekDayLastYear) ?
+  {year: INVALID_VALUE, week: INVALID_VALUE} : {year: year - 1, week: 52};
+}
+
+function containsSaturdayInFirstWeek(date) {
+  const firstDay = date.clone().startOf(YEAR);
+
+  return firstDay.day() >= SUNDAY && firstDay.day() <= SATURDAY;
+}
+
+function getFirstEpiWeekDay(oriDate) {
+  const date = oriDate.clone();
+
+  if (containsSaturdayInFirstWeek(date)) {
+    return date.startOf(YEAR);
+  }
+
+  return date.startOf(YEAR).day(NEXT_SUNDAY);
+}
---
+++ src/index.spec.js
@@ -0,0 +1,54 @@
+import epi from "./index";
+
+describe('epi week', () => {
+  it('should calculate epi week', () => {
+    expect(epi('2006-12-31')).toEqual({year: 2007, week: 1});
+    expect(epi('2007-01-07')).toEqual({year: 2007, week: 2});
+    expect(epi('2007-07-01')).toEqual({year: 2007, week: 27});
+    expect(epi('2007-07-07')).toEqual({year: 2007, week: 27});
+    expect(epi('2007-12-29')).toEqual({year: 2007, week: 52});
+
+    expect(epi('2007-12-30')).toEqual({year: 2008, week: 1});
+    expect(epi('2008-01-06')).toEqual({year: 2008, week: 2});
+    expect(epi('2008-07-06')).toEqual({year: 2008, week: 28});
+    expect(epi('2008-07-12')).toEqual({year: 2008, week: 28});
+    expect(epi('2008-12-20')).toEqual({year: 2008, week: 51});
+    expect(epi('2008-12-27')).toEqual({year: 2008, week: 52});
+    expect(epi('2008-12-28')).toEqual({year: -1, week: -1});
+
+    expect(epi('2009-01-02')).toEqual({year: -1, week: -1});
+    expect(epi('2009-01-04')).toEqual({year: 2009, week: 1});
+    expect(epi('2009-12-27')).toEqual({year: 2009, week: 52});
+    expect(epi('2010-01-02')).toEqual({year: 2009, week: 52});
+
+    expect(epi('2010-01-03')).toEqual({year: 2010, week: 1});
+    expect(epi('2010-12-26')).toEqual({year: 2010, week: 52});
+    expect(epi('2011-01-01')).toEqual({year: 2010, week: 52});
+
+    expect(epi('2011-01-02')).toEqual({year: 2011, week: 1});
+    expect(epi('2011-12-31')).toEqual({year: 2011, week: 52});
+
+    expect(epi('2012-01-01')).toEqual({year: 2012, week: 1});
+    expect(epi('2012-12-29')).toEqual({year: 2012, week: 52});
+
+    expect(epi('2012-12-30')).toEqual({year: 2013, week: 1});
+    expect(epi('2013-01-05')).toEqual({year: 2013, week: 1});
+    expect(epi('2013-12-28')).toEqual({year: 2013, week: 52});
+
+    expect(epi('2013-12-29')).toEqual({year: 2014, week: 1});
+    expect(epi('2014-01-04')).toEqual({year: 2014, week: 1});
+    expect(epi('2014-12-28')).toEqual({year: -1, week: -1});
+    expect(epi('2014-12-31')).toEqual({year: -1, week: -1});
+
+    expect(epi('2015-01-03')).toEqual({year: -1, week: -1});
+    expect(epi('2015-01-04')).toEqual({year: 2015, week: 1});
+    expect(epi('2015-12-31')).toEqual({year: 2015, week: 52});
+
+    expect(epi('2016-05-05')).toEqual({year: 2016, week: 18});
+    expect(epi('2016-11-12')).toEqual({year: 2016, week: 45});
+    expect(epi('2016-11-13')).toEqual({year: 2016, week: 46});
+    expect(epi('2016-11-14')).toEqual({year: 2016, week: 46});
+    expect(epi('2016-01-03')).toEqual({year: 2016, week: 1});
+    expect(epi('2016-01-01')).toEqual({year: 2015, week: 52});
+  });
+});
